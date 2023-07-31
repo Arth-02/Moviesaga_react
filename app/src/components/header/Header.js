b8a6da195c13a19cae43288396857d4f0d10c7d6
Arth-02 <123308770+Arth-02@users.noreply.github.com>
2023-07-31 13:39:50 +0530

    Add files via upload

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -1,13 +1,18 @@
-import React, { useState , useContext } from 'react'
-import { Link, useLocation , useNavigate } from 'react-router-dom'
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import React, { useState, useContext } from "react";
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  createSearchParams,
+} from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
-import Account from '../account/Account';
-import { MyContext } from '../../MyContext';
-import './header.css';
-import { Button } from '@mui/material';
+import Account from "../account/Account";
+import { MyContext } from "../../MyContext";
+import "./header.css";
+import { Button } from "@mui/material";
 
 const Header = () => {
   const { isAuthenticated } = useContext(MyContext);
@@ -16,41 +21,120 @@ const Header = () => {
 
   const [searchTerm, setSearchTerm] = useState(null);
 
-  const handleSearch = () => {
-    console.log(searchTerm);
-    searchTerm && navigate(`/search?query=${searchTerm}`);
-  }
+  const handleSearch = (e) => {
+    if ((e.key === "Enter" || e.button === 0) && searchTerm) {
+      setSearchTerm(null);
+
+      navigate({
+        pathname: "/search",
+        search: `?${createSearchParams({ q: searchTerm })}`,
+      });
+    }
+  };
 
   return (
     <>
-      <header className='header'>
-        <div className='logo'>
-          <h3><Link to={'..'}>MovieSaga</Link></h3>
+      <header className="header">
+        <div className="logo">
+          <h3>
+            <Link to={".."}>MovieSaga</Link>
+          </h3>
         </div>
-        <div className='navtabs'>
-        <span><Link to={'..'} className={location.pathname === '/' ? 'active tabs' : 'tabs'} >Home</Link></span>
-          <span><Link to={'/trending/movie'} className={location.pathname === '/trending/movie' ? 'active tabs' : 'tabs'} >Movies</Link></span>
-          <span><Link to={'/trending/tv'} className={location.pathname === '/trending/tv' ? 'active tabs' : 'tabs'} >Tv Shows</Link></span>
-          <span><Link to={'..'} className={window.pathname === '/categories' ? 'active tabs' : 'tabs'} >Categories</Link></span>
-          <span><Link to={'/trending/all'} className={location.pathname === '/trending/all' ? 'active tabs' : 'tabs'} >Trending</Link></span>
+        <div className="navtabs">
+          <span>
+            <Link
+              to={".."}
+              className={location.pathname === "/" ? "active tabs" : "tabs"}
+            >
+              Home
+            </Link>
+          </span>
+          <span>
+            <Link
+              to={"/trending/movie"}
+              className={
+                location.pathname === "/trending/movie" ? "active tabs" : "tabs"
+              }
+            >
+              Movies
+            </Link>
+          </span>
+          <span>
+            <Link
+              to={"/trending/tv"}
+              className={
+                location.pathname === "/trending/tv" ? "active tabs" : "tabs"
+              }
+            >
+              Tv Shows
+            </Link>
+          </span>
+          <span>
+            <Link
+              to={".."}
+              className={
+                window.pathname === "/categories" ? "active tabs" : "tabs"
+              }
+            >
+              Categories
+            </Link>
+          </span>
+          <span>
+            <Link
+              to={"/trending/all"}
+              className={
+                location.pathname === "/trending/all" ? "active tabs" : "tabs"
+              }
+            >
+              Trending
+            </Link>
+          </span>
         </div>
-        <div className='right-navbar'>
-          <div className='search-box'>
-
-              <IconButton color='inherit' className='search-icon' label='hidden' onClick={handleSearch} > <SearchIcon /> </IconButton>
-              <input type='text' className='search-input' placeholder='Search.... ' name='Search' onChange={(e) => {setSearchTerm(e.target.value)}}/>
-
+        <div className="right-navbar">
+          <div className="search-box">
+            <IconButton
+              color="inherit"
+              className="search-icon"
+              label="hidden"
+              onClick={handleSearch}
+            >
+              {" "}
+              <SearchIcon />{" "}
+            </IconButton>
+            <input
+              type="text"
+              value={searchTerm}
+              className="search-input"
+              placeholder="Search.... "
+              name="Search"
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
+              }}
+              onKeyUp={handleSearch}
+            />
           </div>
-          <div className='user-panel'>
-            <div className='liked-movies'> <IconButton color='inherit'> <FavoriteBorderIcon /> </IconButton> </div>
+          <div className="user-panel">
+            <div className="liked-movies">
+              {" "}
+              <IconButton color="inherit">
+                {" "}
+                <FavoriteBorderIcon />{" "}
+              </IconButton>{" "}
+            </div>
 
-            {isAuthenticated ? <Account/> : <Button variant="text" color='inherit'> <Link to={'/login'}>Sign In</Link></Button>}
-
+            {isAuthenticated ? (
+              <Account />
+            ) : (
+              <Button variant="text" color="inherit">
+                {" "}
+                <Link to={"/login"}>Sign In</Link>
+              </Button>
+            )}
           </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- app/src/pages/search result/SearchResult.jsx
+++ app/src/pages/search result/SearchResult.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './search-result.css'
+import React, { useEffect } from "react";
+import "./search-result.css";
+import { useSearchParams } from "react-router-dom";
+import useFetchData from "../../hooks/useFetchData";
 
 const SearchResult = () => {
-  return (
-    <div>SearchResult</div>
-  )
-}
+  let [searchParams] = useSearchParams();
 
-export default SearchResult
+  const { data, loading, error } = useFetchData(
+    `https://api.themoviedb.org/3/search/multi?api_key=ace3eeed99f6d9d19e61456a520cda0b&&query=${searchParams.get(
+      "q"
+    )}`
+  );
+
+  return (
+    <>
+      {console.log(searchParams.get("q"))}
+      {data && console.log(data)}
+    </>
+  );
+};
+
+export default SearchResult;

3476a7b49de26365cd9c11db59029fe868a7f158
Arth <arthmp2003@gmail.com>
2023-08-08 22:57:30 +0530

    Reduced Image Quality

--- app/src/components/movieCard/MovieCard.js
+++ app/src/components/movieCard/MovieCard.js
@@ -9,7 +9,7 @@ import {Link} from 'react-router-dom';
 
 const MovieCard = (props) => {
 
-  const image_url = "https://image.tmdb.org/t/p/w500";
+  const image_url = "https://image.tmdb.org/t/p/w185";
 
   const [checked, setChecked] = useState(false);
 
